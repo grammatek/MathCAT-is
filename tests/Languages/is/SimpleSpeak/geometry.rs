9f8290abd8a953092b41ed217dac0707c41161b9
Anna Nikulásdóttir <anna@grammatek.com>
2024-10-22 09:11:59 +0000

    translate geometry

--- tests/Languages/is/SimpleSpeak/geometry.rs
+++ tests/Languages/is/SimpleSpeak/geometry.rs
@@ -5,23 +5,22 @@ use crate::common::*;
 #[test]
 fn arc() {
   let expr = "<math>  <mover><mrow><mi>B</mi><mi>C</mi></mrow><mo>⌒</mo></mover> </math>";
-  test("en", "SimpleSpeak", expr, "arc cap b cap c");
+  test("is", "SimpleSpeak", expr, "bogi stórt b stórt c");  // en. 'arc' = 1. 'örk', 'hringbogi', 'bogi' 2. 'bogi', 'ferilbogi'
 }
 
 #[test]
 fn ray() {
   let expr = "<math> <mover><mrow><mi>X</mi><mi>Y</mi></mrow><mo>&#xAF;</mo></mover> </math>";
-  test("en", "SimpleSpeak", expr, "line segment cap x cap y");
-}
+  test("is", "SimpleSpeak", expr, "strik stórt x stórt y"); // en. 'line segment' = 'línustrik', 'strik'
 
 #[test]
 fn arc_mtext() {
   let expr = "<math> <mover><mtext>BC</mtext><mo>⌒</mo></mover> </math>";
-  test("en", "SimpleSpeak", expr, "arc cap b cap c");
+  test("is", "SimpleSpeak", expr, "bogi stórt b stórt c");
 }
 
 #[test]
 fn ray_mtext() {
   let expr = "<math> <mover><mtext>XY</mtext><mo>→</mo></mover> </math>";
-  test("en", "SimpleSpeak", expr, "ray cap x cap y");
+  test("is", "SimpleSpeak", expr, "háflína stórt x stórt y"); // en. 'ray'
 }
