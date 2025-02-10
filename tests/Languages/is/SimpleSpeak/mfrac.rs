2f0bc2a1d5185354458168dad861123f0a267fb2
Anna Nikulásdóttir <anna@grammatek.com>
2025-02-10 15:04:10 +0000

    tests mfrac round1

--- tests/Languages/is/SimpleSpeak/mfrac.rs
+++ tests/Languages/is/SimpleSpeak/mfrac.rs
@@ -8,7 +8,7 @@ fn common_fraction_half() {
     let expr = "<math>
                     <mfrac> <mn>1</mn> <mn>2</mn> </mfrac>
                 </math>";
-    test("en", "SimpleSpeak", expr, "1 half");
+    test("is", "SimpleSpeak", expr, "1 hálfur");
 }
 
 #[test]
@@ -16,7 +16,7 @@ fn common_fraction_thirds() {
     let expr = "<math>
                     <mfrac> <mn>2</mn> <mn>3</mn> </mfrac>
                 </math>";
-    test("en", "SimpleSpeak", expr, "2 thirds");
+    test("is", "SimpleSpeak", expr, "2 þriðju");
 }
 
 #[test]
@@ -24,7 +24,7 @@ fn common_fraction_tenths() {
     let expr = "<math>
                     <mfrac> <mn>17</mn> <mn>10</mn> </mfrac>
                 </math>";
-    test("en", "SimpleSpeak", expr, "17 tenths");
+    test("is", "SimpleSpeak", expr, "17 tíundu");
 }
 
 #[test]
@@ -33,7 +33,7 @@ fn not_SimpleSpeak_common_fraction_tenths() {
     let expr = "<math>
                     <mfrac> <mn>89</mn> <mn>10</mn> </mfrac>
                 </math>";
-    test("en", "SimpleSpeak", expr, "89 over 10,");
+    test("is", "SimpleSpeak", expr, "89 deilt með 10,");
 }
 
 #[test]
@@ -49,7 +49,7 @@ fn non_simple_fraction() {
         </mrow>
     </math>
                             ";
-    test("en", "SimpleSpeak", expr, "fraction, x plus y, over, x minus y, end fraction;");
+    test("is", "SimpleSpeak", expr, "brotið, x plús y, deilt með, x mínus y, brot endar;");
 }
 
 #[test]
@@ -65,7 +65,7 @@ fn nested_fraction() {
         </mrow>
     </math>
                             ";
-    test("en", "SimpleSpeak", expr, "fraction, x plus, fraction, 1 over y, end fraction; over, x minus y, end fraction;");
+    test("is", "SimpleSpeak", expr, "brotið, x plús, brotið, 1 deilt með y, brot endar; deilt með, x mínus y, brot endar;");
 }
 
 
@@ -82,7 +82,7 @@ fn deeply_nested_fraction_msqrt() {
         </mrow>
     </math>
                             ";
-    test("en", "SimpleSpeak", expr, "fraction, x plus, the square root of 1 over y, end root; over, x minus y, end fraction;");
+    test("is", "SimpleSpeak", expr, "brotið, x plús, ferningsrótin af 1 deilt með y, lok ferningsrótar; deilt með, x mínus y, brot endar;");
 }
 
 #[test]
@@ -98,7 +98,7 @@ fn deeply_nested_fraction_mrow_msqrt() {
         </mrow>
     </math>
                             ";
-    test("en", "SimpleSpeak", expr, "fraction, x plus, the square root of 2 plus 1 over y, end root; over, x minus y, end fraction;");
+    test("is", "SimpleSpeak", expr, "brotið, x plús, ferningsrótin af 2 plús 1 deilt með y, lok ferningsrótar; deilt með, x mínus y, brot endar;");
 }
 
 #[test]
@@ -114,7 +114,7 @@ fn numerator_simple_fraction() {
         </mrow>
     </math>
                             ";
-    test("en", "SimpleSpeak", expr, "fraction, x over, x minus y, end fraction;");
+    test("is", "SimpleSpeak", expr, "brotið, x deilt með, x mínus y, brot endar;");
 }
 
 #[test]
@@ -127,7 +127,7 @@ fn denominator_simple_fraction() {
         </mfrac>
     </math>
                             ";
-    test("en", "SimpleSpeak", expr, "fraction, x minus y, over x, end fraction;");
+    test("is", "SimpleSpeak", expr, "brotið, x mínus y, deilt með x, brot endar;");
 }
 
 
@@ -137,7 +137,7 @@ fn mixed_number() {
                     <mn>3</mn>
                     <mfrac> <mn>1</mn> <mn>2</mn> </mfrac>
                 </math>";
-    test("en", "SimpleSpeak", expr, "3 and 1 half");
+    test("is", "SimpleSpeak", expr, "3 og 1 hálfur");
 }
 
 #[test]
@@ -147,7 +147,7 @@ fn explicit_mixed_number() {
                     <mo>&#x2064;</mo>
                     <mfrac> <mn>1</mn> <mn>8</mn> </mfrac>
                 </math>";
-    test("en", "SimpleSpeak", expr, "3 and 1 eighth");
+    test("is", "SimpleSpeak", expr, "3 og 1 áttundi");
 }
 
 #[test]
@@ -156,7 +156,7 @@ fn mixed_number_big() {
                     <mn>3</mn>
                     <mfrac> <mn>7</mn> <mn>83</mn> </mfrac>
                 </math>";
-    test("en", "SimpleSpeak", expr, "3 and 7 eighty thirds");
+    test("is", "SimpleSpeak", expr, "3 og 7 áttugustu og þriðju");
 }
 
 #[test]
@@ -164,7 +164,7 @@ fn simple_text() {
     let expr = "<math>
     <mfrac> <mi>rise</mi> <mi>run</mi> </mfrac>
                 </math>";
-    test("en", "SimpleSpeak", expr, "rise over run,");
+    test("is", "SimpleSpeak", expr, "rise deilt með run,");
 }
 
 #[test]
@@ -177,7 +177,7 @@ fn number_and_text() {
                 <mn>3</mn><mtext>gallons</mtext></mrow>
             </mfrac>
         </math>";
-    test("en", "SimpleSpeak", expr, "fraction, 2 miles, over, 3 gallons, end fraction;");
+    test("is", "SimpleSpeak", expr, "brotið, 2 miles, deilt með, 3 gallons, brot endar;");
 }
 
 
@@ -201,7 +201,7 @@ fn nested_simple_fractions() {
                 </mfrac>
                 </mrow>
             </math>";
-    test("en", "SimpleSpeak", expr, "fraction, 1 half, over, 2 thirds, end fraction;");
+    test("is", "SimpleSpeak", expr, "brotið, 1 hálfur, deilt með, 2 þriðju, brot endar;");
 }
 
 #[test]
@@ -212,5 +212,5 @@ fn binomial() {
                     <mfrac linethickness='0'> <mn>7</mn> <mn>3</mn> </mfrac>
                     <mo>)</mo>
                 </math>";
-    test("en", "SimpleSpeak", expr, "2 times 7 choose 3");
+    test("is", "SimpleSpeak", expr, "2 sinnum 7 valið 3"); // ICELANDIC: what does that mean, 'choose 3'?
 }
