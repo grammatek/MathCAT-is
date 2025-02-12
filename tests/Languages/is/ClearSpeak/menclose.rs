ffe5fdc9b8b50f8431a82da658af0fdbcc97f6b1
Anna Nikulásdóttir <anna@grammatek.com>
2025-02-12 13:49:09 +0000

    test menclose

--- tests/Languages/is/ClearSpeak/menclose.rs
+++ tests/Languages/is/ClearSpeak/menclose.rs
@@ -5,7 +5,7 @@ fn menclose_actuarial() {
     let expr = "<math>
                     <menclose notation='actuarial'>  <mn>3</mn><mo>+</mo><mn>2</mn><mi>i</mi> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "actuarial symbol, enclosing 3 plus 2 i end enclosure,");
+    test("is", "ClearSpeak", expr, "tryggingamerki, umlykja 3 plús 2 i umlykja endar,");
 }
 
 #[test]
@@ -13,7 +13,7 @@ fn menclose_box() {
     let expr = "<math>
                     <menclose notation='box circle'>  <mn>3</mn><mo>+</mo><mn>2</mn><mi>i</mi> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "box, circle, enclosing 3 plus 2 i end enclosure,");
+    test("is", "ClearSpeak", expr, "kassi, hringur, umlykja 3 plús 2 i umlykja endar,");
 }
 
 #[test]
@@ -21,7 +21,7 @@ fn menclose_left() {
     let expr = "<math>
                     <menclose notation='left'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "line on left, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "lína vinstra megin, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -29,7 +29,7 @@ fn menclose_right() {
     let expr = "<math>
                     <menclose notation='right'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "line on right, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "lína hægra megin, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -37,7 +37,7 @@ fn menclose_top_bottom() {
     let expr = "<math>
                     <menclose notation='top bottom'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "line on top, bottom, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "lína fyrir ofan, fyrir neðan, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -45,7 +45,7 @@ fn menclose_updiagonalstrike() {
     let expr = "<math>
                     <menclose notation='updiagonalstrike'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "up diagonal, cross out, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "upp á ská, útstrikun, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -53,7 +53,7 @@ fn menclose_downdiagonalstrike() {
     let expr = "<math>
                     <menclose notation='downdiagonalstrike'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "down diagonal, cross out, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "niður á ská, útstrikun, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -61,7 +61,7 @@ fn menclose_cross_out() {
     let expr = "<math>
                     <menclose notation='updiagonalstrike downdiagonalstrike'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "x, cross out, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "x, útstrikun, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -69,7 +69,7 @@ fn menclose_vertical_horizontal_strike() {
     let expr = "<math>
                     <menclose notation='verticalstrike horizontalstrike'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "vertical, horizontal, cross out, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "lóðrétt, lárétt, útstrikun, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -77,7 +77,7 @@ fn menclose_leftarrow() {
     let expr = "<math>
                     <menclose notation='leftarrow'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "left arrow, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "ör til vinstri, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -85,7 +85,7 @@ fn menclose_right_up_down_arrow() {
     let expr = "<math>
                     <menclose notation=' rightarrow downarrow  uparrow  '> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "up arrow, down arrow, right arrow, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "ör upp, ör niður, ör til hægri, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -93,7 +93,7 @@ fn menclose_northeastarrow() {
     let expr = "<math>
                     <menclose notation='northeastarrow'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "northeast arrow, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "ör í norðaustur, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -101,7 +101,7 @@ fn menclose_other_single_arrows() {
     let expr = "<math>
                     <menclose notation='northwestarrow southwestarrow southeastarrow'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "southeast arrow, southwest arrow, northwest arrow, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "ör í suðaustur, ör í suðvestur, ör í norðvestur, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -109,7 +109,7 @@ fn menclose_northwestsoutheastarrow() {
     let expr = "<math>
                     <menclose notation='northwestsoutheastarrow'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "double ended down diagonal arrow, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "tvíátta ör niður á ská, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -117,7 +117,7 @@ fn menclose_other_double_arrows() {
     let expr = "<math>
                     <menclose notation='updownarrow leftrightarrow northeastsouthwestarrow'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "double ended vertical arrow, double ended horizontal arrow, double ended up diagonal arrow, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "tvíátta lóðrétt ör, tvíátta lárétt ör, tvíátta ör upp á ská, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -125,7 +125,7 @@ fn menclose_madrub() {
     let expr = "<math>
                     <menclose notation='madrub'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "arabic factorial symbol, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "arabískt aðfeldismerki, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -133,7 +133,7 @@ fn menclose_phasorangle() {
     let expr = "<math>
                     <menclose notation='phasorangle'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "phasor angle, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "fasorhorn, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -141,7 +141,7 @@ fn menclose_circle_phasorangle() {
     let expr = "<math>
                     <menclose notation='phasorangle circle'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "circle, phasor angle, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "hringur, fasorhorn, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -149,7 +149,7 @@ fn menclose_longdiv() {
     let expr = "<math>
                     <menclose notation='longdiv'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "long division symbol, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "löng deiling, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -157,7 +157,7 @@ fn menclose_longdiv_default() {
     let expr = "<math>
                     <menclose> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "long division symbol, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "löng deiling, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -165,7 +165,7 @@ fn menclose_longdiv_empty_string() {
     let expr = "<math>
                     <menclose notation=''> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "long division symbol, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "löng deiling, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -173,7 +173,7 @@ fn menclose_longdiv_whitespace_string() {
     let expr = "<math>
                     <menclose notation='  '> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "long division symbol, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "löng deiling, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -181,7 +181,7 @@ fn menclose_radical() {
     let expr = "<math>
                     <menclose notation='radical'> <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "ClearSpeak", expr, "square root, enclosing 3 halves end enclosure,");
+    test("is", "ClearSpeak", expr, "ferningsrótin, umlykja 3 aðrir umlykja endar,");
 }
 
 #[test]
@@ -189,5 +189,5 @@ fn simple_speak_menclose_top_bottom() {
     let expr = "<math>
                     <menclose notation='top bottom'>  <mfrac><mn>3</mn><mn>2</mn></mfrac> </menclose>
                 </math>";
-    test("en", "SimpleSpeak", expr, "line on top, bottom, enclosing 3 halves end enclosure,");
+    test("is", "SimpleSpeak", expr, "lína fyrir ofan, fyrir neðan, umlykja 3 aðrir umlykja endar,");
 }
